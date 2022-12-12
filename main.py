48a38a3227690169151e5b26b3de70f53c81c7e2
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-12-12 23:42:08 +0800

    Form in contact.html is now functional and will send details to email

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 app = Flask(__name__)
 
@@ -24,6 +26,31 @@ def post(num):
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'POST':
+        data = request.form
+
+        EMAIL = "zoulaimi@hotmail.com"
+        PASSWORD = os.environ.get("PASSWORD")
+        SMTP_SERVER = {
+            "gmail": "smtp.gmail.com",
+            "yahoo": "smtp.mail.yahoo.com",
+            "hotmail": "smtp-mail.outlook.com",
+        }
+        with smtplib.SMTP(f"{SMTP_SERVER['hotmail']}") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=EMAIL,
+                msg=f'Subject:New Message\n\n'
+                    f'Name: {data["name"]}\nEmail: {data["email"]}\nPhone: {data["phone"]}\nMessage: {data["message"]}'
+            )
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 if __name__ == "__main__":
